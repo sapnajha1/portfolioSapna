704fc49d272161c71c61fde1fe558efff226e035
Shalini <106245097+shalini8979@users.noreply.github.com>
2023-07-21 15:29:19 +0530

    Update f_desktop.dart

--- lib/focus_screens/f_desktop.dart
+++ lib/focus_screens/f_desktop.dart
@@ -1,21 +1,20 @@
 // import 'package:english/screen/aap_bar.dart';
+import 'package:eng_clone_2/components/article_Img.dart';
+import 'package:eng_clone_2/components/focus.dart';
+import 'package:eng_clone_2/constWidget/textWidget.dart';
+import 'package:eng_clone_2/constants/colors.dart';
+import 'package:eng_clone_2/screens/article_content.dart';
 import 'package:flutter/material.dart';
-import '../article_content.dart';
-import '../components/Article.dart';
-import '../components/Focus.dart';
-import '../const/color.dart';
-import '../constWidget/textwidget.dart';
-
 
 
 class f_DesktopPage extends StatefulWidget {
 
 
   f_DesktopPage({required this.article_name_in, required this.selected_index,required this.selected_index2,required this.textsize});
-  final int article_name_in;
-  final int selected_index;
-  final int selected_index2;
-  final double textsize;
+  int article_name_in;
+  int selected_index;
+  int selected_index2;
+  double textsize;
   @override
   State<f_DesktopPage> createState() => _f_DesktopPageState();
 }
@@ -25,11 +24,11 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
   // Color btn_col=Colors.white;
 
   Color btn_col = Colors.white;
-  // double textsize=20;
-  // int selected_index=3;
-  // int article_name_in=0;
-  // int selected_index2=1;
-
+ 
+  // int articleIndex = widget.article_name_in;
+  //   int widget.selected_index = widget.selected_index;
+  //   int widget.selected_index2 = widget.selected_index2;
+  // double widget.textsize = textsize;
 
 
 
@@ -37,10 +36,8 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
 
   @override
   Widget build(BuildContext context) {
-    int articleIndex = widget.article_name_in;
-    int selectedIndex = widget.selected_index;
-    int selectedIndex2 = widget.selected_index2;
-    double textsizeIndex = widget.textsize;
+    
+    
 
     var mq= MediaQuery.of(context).size;
     return Scaffold(
@@ -64,11 +61,11 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
                       children: [
 
                         // Article image
-                        Article_image(height:mq.height*0.15,width:mq.width*0.15,imgurl:articles[articleIndex]['article_image']),
+                        Article_image(height:mq.height*0.15,width:mq.width*0.15,imgurl:articles[widget.article_name_in]['article_image']),
 
                         // Article name
                         const SizedBox(width:0),
-                        textwidget(articles[articleIndex]['article_name'],25, FontWeight.bold, textcolor),
+                        textwidget(articles[widget.article_name_in]['article_name'],25, FontWeight.bold, textcolor),
 
                         // Article in Focus mode
                         const SizedBox(width:0),
@@ -98,20 +95,20 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
                                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                         children:[
                                           FloatingActionButton(elevation:0.0,backgroundColor:
-                                          selectedIndex==1?Colors.lightGreen:Colors.white,
-                                              onPressed:(){setState(() {selectedIndex=1;});},child: textwidget("1", 20, FontWeight.w300, textcolor)),
+                                          widget.selected_index==1?Colors.lightGreen:Colors.white,
+                                              onPressed:(){setState(() {widget.selected_index=1;});},child: textwidget("1", 20, FontWeight.w300, textcolor)),
                                           FloatingActionButton(elevation:0.0,backgroundColor:
-                                          selectedIndex==2?Colors.lightGreen:Colors.white,
-                                              onPressed:(){setState(() {selectedIndex=2;});},child: textwidget("2", 20, FontWeight.w300, textcolor)),
+                                          widget.selected_index==2?Colors.lightGreen:Colors.white,
+                                              onPressed:(){setState(() {widget.selected_index=2;});},child: textwidget("2", 20, FontWeight.w300, textcolor)),
                                           FloatingActionButton(elevation:0.0,backgroundColor:
-                                          selectedIndex==3?Colors.lightGreen:Colors.white,
-                                              onPressed:(){setState(() {selectedIndex=3;});},child: textwidget("3", 20, FontWeight.w300, textcolor)),
+                                          widget.selected_index==3?Colors.lightGreen:Colors.white,
+                                              onPressed:(){setState(() {widget.selected_index=3;});},child: textwidget("3", 20, FontWeight.w300, textcolor)),
                                           FloatingActionButton(elevation:0.0,backgroundColor:
-                                          selectedIndex==4?Colors.lightGreen:Colors.white,
-                                              onPressed:(){setState(() {selectedIndex=4;});},child: textwidget("4", 20, FontWeight.w300, textcolor)),
+                                          widget.selected_index==4?Colors.lightGreen:Colors.white,
+                                              onPressed:(){setState(() {widget.selected_index=4;});},child: textwidget("4", 20, FontWeight.w300, textcolor)),
                                           FloatingActionButton(elevation:0.0,backgroundColor:
-                                          selectedIndex==5?Colors.lightGreen:Colors.white,
-                                              onPressed:(){setState(() {selectedIndex=5;});},child: textwidget("5", 20, FontWeight.w300, textcolor)),
+                                          widget.selected_index==5?Colors.lightGreen:Colors.white,
+                                              onPressed:(){setState(() {widget.selected_index=5;});},child: textwidget("5", 20, FontWeight.w300, textcolor)),
                                         ],),
                                     ),
                                   ]),
@@ -129,16 +126,16 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
 
                               children:[
                                 FloatingActionButton(elevation: 0.0,backgroundColor:
-                                selectedIndex2==1?Color.fromARGB(255, 113, 168, 47):Colors.white,
-                                    onPressed:(){setState(() {selectedIndex2=1;textsizeIndex=22;});},
+                                widget.selected_index2==1?Color.fromARGB(255, 113, 168, 47):Colors.white,
+                                    onPressed:(){setState(() {widget.selected_index2=1;widget.textsize=22;});},
                                     child: textwidget("A",15, FontWeight.w500, Colors.black)),
                                 FloatingActionButton(elevation: 0.0,backgroundColor:
-                                selectedIndex2==2?Color.fromARGB(255, 113, 168, 47):Colors.white,
-                                    onPressed:(){setState(() {selectedIndex2=2;textsizeIndex=24;});},
+                                widget.selected_index2==2?Color.fromARGB(255, 113, 168, 47):Colors.white,
+                                    onPressed:(){setState(() {widget.selected_index2=2;widget.textsize=24;});},
                                     child: textwidget("A", 20, FontWeight.w500, Colors.black)),
                                 FloatingActionButton(elevation: 0.0,backgroundColor:
-                                selectedIndex2==3?Color.fromARGB(255, 113, 168, 47):Colors.white,
-                                    onPressed:(){setState(() {selectedIndex2=3;textsizeIndex=26;});},
+                                widget.selected_index2==3?Color.fromARGB(255, 113, 168, 47):Colors.white,
+                                    onPressed:(){setState(() {widget.selected_index2=3;widget.textsize=26;});},
                                     child: textwidget("A",23, FontWeight.w500, Colors.black)),
 
                               ],),
@@ -151,8 +148,8 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
                     Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: Container(
-                            child: textwidget(articles[articleIndex]['versions'][selectedIndex-1],
-                                textsizeIndex, FontWeight.w300,Colors.black))
+                            child: textwidget(articles[widget.article_name_in]['versions'][widget.selected_index-1],
+                                widget.textsize, FontWeight.w300,Colors.black))
                     ),
 
                   ],
@@ -169,9 +166,3 @@ class _f_DesktopPageState extends State<f_DesktopPage> {
     );
   }
 }
-
-
-
-
-
-
